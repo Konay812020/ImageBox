99d567514debd06df61b88a21347eb4883e56ce2
konay812020 <konay812020@gmail.com>
2022-09-14 07:22:42 +0630

    imagebox

---
+++ app.js
@@ -0,0 +1,44 @@
+const getimageboxes = document.querySelectorAll(".imagebox");
+// console.log(getimageboxes); //NodeList Array
+
+getimageboxes.forEach(function (getimagebox, idx) {
+    // console.log(getimagebox);
+    // console.log(idx)
+
+    getimagebox.addEventListener("click", function () {
+        showbox(idx)
+    })
+})
+
+
+function showbox(idx) {
+    // console.log(idx);
+
+    getimageboxes.forEach(function (imagebox, curidx) {
+        // console.log("from parameter=", idx);
+        // console.log("from current=", curidx);
+
+        if (idx === curidx) {
+            imagebox.classList.add("show");
+
+            imagebox.addEventListener("click", function (e) {
+                // console.log(e.target)
+                // console.log(e.target.className)
+                if (e.target.className === "btn-close") {
+                    imagebox.classList.remove("show");
+                }
+                if (e.target.className === "btn") {
+                    // const subbtn = imagebox.querySelector(".btn");
+                    const subbtn = getimageboxes[idx].querySelector(".btn");
+                    subbtn.textContent = "Subscribed";
+                }
+            })
+        } else {
+            imagebox.classList.remove("show");
+        }
+    })
+
+}
+
+// const subbtn = getimageboxes[0].querySelector(".btn");
+// console.log(subbtn)
